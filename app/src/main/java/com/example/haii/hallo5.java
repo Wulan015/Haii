acefc382f1f3f5af116123fd1a152768251b9de9
Wulan015 <dariw9023@gmail.com>
2021-10-01 12:16:01 +0700

    update project saya 1

---
+++ app/src/main/java/com/example/haii/hallo5.java
@@ -0,0 +1,14 @@
+package com.example.haii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hallo5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallo5);
+    }
+}
